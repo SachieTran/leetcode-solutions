45cb2a1c84dec492daa4ee817c4de434a8384d10
Abhay Doke <abhaydoke09@gmail.com>
2016-12-25 17:28:27 -0500

    edited rotate_matrix

--- rotate_matrix_by_90_inplace.py
+++ rotate_matrix_by_90_inplace.py
@@ -6,7 +6,7 @@ def dispMatrix(matrix):
 		print ''
 	print ''
 
-def rotateMatrix(matrix):
+def rotateMatrixAntiClockWise(matrix):
 	n = len(matrix)
 	for x in range(len(matrix)/2):
 		for y in range(x, n-x-1): 
@@ -18,8 +18,20 @@ def rotateMatrix(matrix):
 
 	return matrix	
 
+def rotateMatrixClockWise(matrix):
+	n = len(matrix)
+	for x in range(n/2):
+		for y in range(x,n-x-1):
+			temp = matrix[x][y]
+			matrix[x][y] = matrix[n-1-y][x]
+			matrix[n-1-y][x] = matrix[n-1-x][n-1-y]
+			matrix[n-1-x][n-1-y] = matrix[y][n-1-x]
+			matrix[y][n-1-x] = temp
+	return matrix
 
-matrix = [[i+j for i in range(6)] for j in range(6)]
+
+#matrix = [[i+j for i in range(6)] for j in range(6)]
+matrix = [[1,2],[3,4]]
 dispMatrix(matrix)
-dispMatrix(rotateMatrix(matrix))
+dispMatrix(rotateMatrixClockWise(matrix))
 
