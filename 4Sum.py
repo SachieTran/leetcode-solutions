b91a1dc5cbd42ac3b55e8568d0fa8822f3b8e7da
abhaydoke09 <abhaydoke09@gmail.com>
2017-07-17 16:12:39 -0400

    changed solution

--- 4Sum.py
+++ 4Sum.py
@@ -1,21 +1,24 @@
-S = [1, 0, -1, 0, -2, 2]
+S = [-5,-4,-3,-2,-1,0,0,1,2,3,4,5]
+
+target = 0
 S.sort()
 resultSet = []
 target = 0
 listLength = len(S)
 targetListLength = 4
 for i,element in enumerate(S):
+    print resultSet
     if len(resultSet) == 0:
-        if element<=target:
+        if (listLength-i >= targetListLength):
             resultSet.append((element,))
     else:
         set_length = len(resultSet)
-        for i in xrange(set_length):
-            currentList = list(resultSet[i])
+        for j in xrange(set_length):
+            currentList = list(resultSet[j])
             if len(currentList)<4:
                 if (sum(currentList)+element) <= target:
-                    resultSet.append(tuple(list(resultSet[i])+[element]))
-        if (listLength-i >= targetListLength) and (element<=target):
+                    resultSet.append(tuple(list(resultSet[j])+[element]))
+        if (listLength-i >= targetListLength):
             resultSet.append((element,))
 
 finalSet = set()
@@ -23,3 +26,6 @@ for l in resultSet:
     if len(l)==4 and sum(l)==0:
         finalSet.add(l)
 print [list(i) for i in finalSet]
+
+
+                
