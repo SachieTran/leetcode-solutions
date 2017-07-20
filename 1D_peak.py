5e10d893c5740d0e5d9b3622565b3cef403ee00b
Abhay Doke <abhaydoke09@gmail.com>
2017-07-20 00:03:35 -0400

    edited 1d peak.py

--- 1D_peak.py
+++ 1D_peak.py
@@ -1,4 +1,15 @@
 s = [1,2,3,4,4,5,6,3,2,1,3]
 
 def find_peak(a, start, end):
-	
+	middle = (start+end)/2
+	if a[middle-1]<=a[middle] and a[middle+1]<=a[middle]:
+		return a[middle]
+	if start==end:
+		return a[start]
+	else:
+		if a[middle]>a[middle-1] and a[middle]<=a[middle+1]:
+			find_peak(a, middle+1, end)
+		else:
+			find_peak(a, start, middle-1)
+
+print find_peak(s,0,len(s)-1)
