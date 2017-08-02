4bddd80d2631092cd276d4ad47110bc059e8c606
abhaydoke09 <abhaydoke09@gmail.com>
2017-08-02 15:20:04 -0400

    first solution

--- 3Sum.py
+++ 3Sum.py
@@ -1,10 +1,23 @@
-s = [0, -1, 2, -3, 1] 
-hash = {}
-for i in range(len(s)-1):
-	for j in range(i+1,len(s)):
-		if -(s[i]+s[j]) in hash:
-			print s[i], s[j], -(s[i]+s[j])
-		else:
-			hash[s[i]] = 0
+s = [-1, 0, 1, 2, -1, -4]
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        self.hash = {}
+        for i in xrange(len(nums)-1):
+        	for j in range(i+1, len(nums)):
+        		if -(nums[i]+nums[j]) in self.hash.keys():
+        			if sorted([nums[i], nums[j], -(nums[i]+nums[j])]) not in result:
+        				result.append(sorted([nums[i], nums[j], -(nums[i]+nums[j])]))
+        		else:
+        			self.hash[nums[i]] = 0
+        return result
+
+p = Solution()
+print p.threeSum(s)
+
 
 
