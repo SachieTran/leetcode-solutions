b5b3d366ac3d499dbaa9b4b99a945a65bf9a1a21
abhaydoke09 <abhaydoke09@gmail.com>
2017-08-02 14:34:36 -0400

    optimizing the solution

--- 4_key_keyboard.py
+++ 4_key_keyboard.py
@@ -1,15 +1,23 @@
-def maxA(N):
-    max = N
-    if N<=6:
-        return N
-    else:
-        for i in range(N-3,0,-1):
-            curr = maxA(i)*(N-i-1)
-            print i, curr
-            if curr>max:
-                max = curr
+class Solution(object):
+    def printA(self, N):
+        print N
+        max = N
+        if N<=6:
+            return N
+        else:
+            for i in range(N-3,2,-1):
+                if self.d[i]!=-1:
+                    curr = self.d[i]*(N-i-1)
+                else:
+                    curr = self.printA(i)*(N-i-1)
+                    self.d[N] = curr
+                if curr>max:
+                    max = curr
+        return max
 
-    return max
+    def maxA(self, N):
+        self.d = {i:-1 for i in xrange(N+1)}
+        return self.printA(N)
 
-print maxA(11)
-
+s = Solution()
+print s.maxA(19)
