1a6307923164f67766d767f3af0ef6e7e517b7be
abhaydoke09 <abhaydoke09@gmail.com>
2017-08-03 09:17:32 -0400

    started solution

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -1,4 +1,3 @@
-
 class Solution(object):
     def intToRoman(self, s):
 		self.map = {1:'I',	5:'V', 10:'X', 50:'L', 100:'C', 500:'D', 1000:'M', 4:'IV', 9:'IX', 40:'XL', 90:'XC', 400:'CD', 900:'CM'}
@@ -43,7 +42,6 @@ class Solution(object):
 			else:
 				result+=self.map[1]*s
 				s%=1
-
 
 		return result
 
