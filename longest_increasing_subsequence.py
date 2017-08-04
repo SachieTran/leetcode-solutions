d40de3c30f2088e838423bcf65f0003c367a6215
abhaydoke09 <abhaydoke09@gmail.com>
2017-08-04 15:05:10 -0400

    local successful longest increasing subsequence

--- longest_increasing_subsequence.py
+++ longest_increasing_subsequence.py
@@ -3,14 +3,12 @@ def _lis(arr, n):
 		return 1
 	for i in range(n-1, -1, -1):
 		res = _lis(arr,i)
-		if n==4:
-			print arr[i], res
 		if res >= table[n] and arr[n] > arr[i]:
 			table[n] = res+1
 	return table[n]
 
 
-arr = [1,2,3,4,5,6,7]
+arr = [1,1,1,1,1,1,1,1,1,1,1,1]
 table = [1 for i in range(len(arr))]
 _lis(arr, len(arr)-1)
 print max(table)
