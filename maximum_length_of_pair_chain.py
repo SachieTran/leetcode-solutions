32bf67c1953f34eca84c7b9e3f96235dd93dd5cf
abhaydoke09 <abhaydoke09@gmail.com>
2017-08-06 20:48:45 -0400

    local successful refinement

--- maximum_length_of_pair_chain.py
+++ maximum_length_of_pair_chain.py
@@ -3,6 +3,7 @@ class Solution(object):
 	def findLongestChain(self, pairs):
 		self.sorted_pairs = sorted(pairs, key = lambda x : (x[0], x[1]))
 		self.table = [1 for i in range(len(self.sorted_pairs))]
+		self.processd = {}
 		self.longestPair(len(self.sorted_pairs)-1)
 		return max(self.table)
 
@@ -11,7 +12,11 @@ class Solution(object):
 			return 1
 		else:
 			for i in range(n-1,-1,-1):
-				res = self.longestPair(i)
+				if i in self.processd:
+					res = self.processd[i]
+				else:	
+					res = self.longestPair(i)
+					self.processd[i] = res
 				if res >= self.table[n] and self.sorted_pairs[i][1]< self.sorted_pairs[n][0]:
 					self.table[n] = self.table[i] + 1
 		return self.table[n]
@@ -19,7 +24,7 @@ class Solution(object):
 
 
 
-s = [[1,2], [2,4], [2,3], [3,4]]
+#s = [[1,2], [2,4], [2,3], [3,4]]
 s = [[5, 24], [15, 25], [27, 40], [50, 60]]
 p = Solution()
 print p.findLongestChain(s)
