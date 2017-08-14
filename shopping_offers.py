a851ea48c618b82916fb8087e2c83a9a5e56f83a
abhaydoke09 <abhaydoke09@gmail.com>
2017-08-14 15:25:18 -0400

    local okay

--- shopping_offers.py
+++ shopping_offers.py
@@ -6,10 +6,21 @@ class Solution(object):
         :type needs: List[int]
         :rtype: int
         """
-        self.price = price
-        self.special = special
-        self.needs = needs
-        self.getValidOffers()
+
+        def getLowestShoppingPrice(price, special, needs, i):
+        	if i == len(special):
+        		return sum(map(lambda x, y: x*y, needs, price))
+        	result = getLowestShoppingPrice(price, special, needs, i+1)
+        	for j in range(len(price)):
+        		needs[j]-=special[i][j]
+        	if all(need>=0 for need in needs):
+        		result = min(result, special[i][-1] + getLowestShoppingPrice(price, special, needs, i))
+        	for j in range(len(price)):
+        		needs[j]+=special[i][j]
+
+        	return result
+
+        print getLowestShoppingPrice(price, special, needs, 0)
 
     
 
