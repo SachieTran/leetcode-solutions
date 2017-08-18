5f0829643ebf1cb568873d0b8d9cde469b5e2b31
abhaydoke09 <abhaydoke09@gmail.com>
2017-08-18 15:31:24 -0400

    graph display okay

--- shortest-path.py
+++ shortest-path.py
@@ -1,7 +1,11 @@
+import Queue
+import networkx as nx
+import matplotlib.pyplot as plt
+
 class Node(object):
 	def __init__(self, id):
 		self.id = id
-		self.adjascent = {}
+		self.adjascent = {id:0}
 
 	def displayNode(self):
 		print 'Node -->', self.id
@@ -29,8 +33,41 @@ class Graph(object):
 		self.nodes[id2].adjascent[id1] = dist
 
 	def displayGraph(self):
-		for node in self.nodes:
-			self.nodes[node].displayNode()
+		G = nx.Graph()
+		for nodeId in self.nodes:
+			G.add_node(nodeId)
+		for nodeId in self.nodes:
+			for adjascentNodeId in self.nodes[nodeId].adjascent.keys():
+				G.add_edge(nodeId, adjascentNodeId, weight=self.nodes[nodeId].adjascent[adjascentNodeId])
+		nx.draw(G, with_labels=True)
+		plt.show()
+
+
+
+
+
+
+	def shortestPath(self, id1, id2):
+		if id1==id2:
+			return 'You want to go there where you already are'
+		shortest_path = []
+		distances = {id:float("inf") for id in self.nodes}
+		visited = {id1:float("inf")}
+		Q = Queue.PriorityQueue()
+		Q.put((0,id1))
+
+		while not Q.empty():
+			current = Q.get()
+			currentNodeId, currentNodeDist = current[1], current[0]
+			if currentNodeId == id2:
+					print 'Shortest path from %s to %s is %f',id1,id2,currentNodeDist
+					break
+			visited[currentNodeId] = currentNodeDist
+			for adjascentNodeId in self.nodes[currentNodeId].adjascent.keys():
+				if adjascentNodeId not in visited:
+					if distances[adjascentNodeId] > (currentNodeDist + self.nodes[currentNodeId].adjascent[adjascentNodeId]):
+						distances[adjascentNodeId] = currentNodeDist + self.nodes[currentNodeId].adjascent[adjascentNodeId]
+					Q.put((distances[adjascentNodeId], adjascentNodeId))
 
 
 
@@ -49,3 +86,4 @@ g.addEdge(3,4,4)
 g.addEdge(3,5,2)
 
 g.displayGraph()
+g.shortestPath(1,5)
