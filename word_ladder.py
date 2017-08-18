746ddc29a69e963b42c60f8351deb84221d03fe9
abhaydoke09 <abhaydoke09@gmail.com>
2017-08-18 17:51:01 -0400

    word ladder local okay

---
+++ word_ladder.py
@@ -0,0 +1,51 @@
+import Queue
+
+dictionary = ['POON', 'PLEE', 'SAME', 'POIE', 'PLEA', 'PLIE', 'POIN']
+
+def isAdjascent(word1, word2):
+	dist = 0
+	for i in range(len(word1)):
+		if word1[i]!=word2[i]:
+			dist+=1
+
+	return dist==1
+
+
+adj_map = {}
+
+for word in dictionary:
+	adj_map[word] = []
+	for word2 in dictionary:
+		if isAdjascent(word, word2) and word!=word2:
+			adj_map[word].append(word2)
+
+
+visited = {}
+min_dist = {word:float("inf") for word in dictionary}
+Q = Queue.PriorityQueue()
+
+start = 'TOON'
+target = 'PLEA'
+
+Q.put((0,start))
+
+while not Q.empty():
+	queueEnd = Q.get()
+	curentDist, currentWord = queueEnd[0], queueEnd[1]
+	visited[currentWord] = curentDist
+	if currentWord==target:
+		print 'Target reached with distance', curentDist
+		break 
+	for word in dictionary:
+		if isAdjascent(currentWord, word) and min_dist[word]>(curentDist+1) and word not in visited:
+			print currentWord
+			Q.put((curentDist+1, word))
+
+
+
+
+
+
+
+
+
