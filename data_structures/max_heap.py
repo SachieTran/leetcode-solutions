2a0493b6784aa1c3c7996efc44d5d1378d217604
abhaydoke09 <abhaydoke09@gmail.com>
2017-08-23 11:22:22 -0400

    added heapify function

--- data_structures/max_heap.py
+++ data_structures/max_heap.py
@@ -1,2 +1,26 @@
 ## Max - Heap
-a = [2,1,3,5,1,2,3,6,8,5,3]
+arr = [2,1,3,5,1,2,3,6,8,5,3]
+
+def heapify(i, n):
+	largest = i
+	l = i*2+1
+	r = i*2+2
+
+	if l<n and arr[l]>arr[i]:
+		largest = l
+
+	if r<n and arr[r]>arr[i]:
+		largest = r
+
+	if largest!=i:
+		temp = arr[i]
+		arr[i] = arr[largest]
+		arr[largest] = temp
+		heapify(largest, n)
+
+
+print arr
+heapify(0, len(arr))
+print arr
+
+
