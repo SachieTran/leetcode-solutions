7b07fb4c57fe31dfa7c9c1bcec9d0e5357de74e0
abhaydoke09 <abhaydoke09@gmail.com>
2017-08-29 15:12:30 -0400

    started continuous_subarray_sum

---
+++ continuous_subarray_sum.py
@@ -0,0 +1,30 @@
+class Solution(object):
+	def checkSubarraySum(self, nums, k):
+	    """
+	    :type nums: List[int]
+	    :type k: int
+	    :rtype: bool
+	    """
+	    current_sum = nums[0]
+	    start = 0
+	    for i in xrange(1, len(nums)):
+	    	current_sum+=nums[i]
+	    	while(current_sum>k and start<i):
+	    		current_sum-=nums[start]
+	    		start+=1
+
+	    	if current_sum==k:
+	    		print 'subarray found from ',start, 'to', i
+	    		return
+
+	    if current_sum==k:
+	    	print 'subarray found from ',start, 'to', i
+	    else:
+	    	print 'No subarray with sum',k,'found'
+
+
+s = Solution()
+a = [15, 2, 4, 8, 9, 5, 10, 23]
+sum = 12
+s.checkSubarraySum(a,sum)
+
