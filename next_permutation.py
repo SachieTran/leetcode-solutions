a539f827be2fc879edd6830319b8222bc0695384
Abhay Doke <abhaydoke09@gmail.com>
2017-09-07 14:04:01 -0400

    added rotate function

--- next_permutation.py
+++ next_permutation.py
@@ -1,5 +1,12 @@
 a = [1,3,2]
 
+def rotate(a,n):
+	for i in range(n):
+		temp = a[0]
+		for j in range(len(a)-1):
+			a[j] = a[j+1]
+		a[-1] = temp
+
 def nextPermutation(a):
 	pivot=len(a)-1
 	while pivot>0 and a[pivot]<a[pivot-1]:
@@ -13,15 +20,15 @@ def nextPermutation(a):
 		right_target-=1
 	right_target_val = a[right_target]
 	print a, a[left_target], a[pivot], a[right_target]
-	for i in range(right_target+1, len(a)):
-		a[i-1] = a[i]
-	a[len(a)-1] = a[left_target]
+	for i in range(right_target, pivot, -1):
+		a[i] = a[i-1]
+	
+	a[pivot] = a[left_target]
 	a[left_target] = right_target_val
 	print a
 
 
 
-
-
-
-nextPermutation(a)
+print a
+rotate(a,1)
+print a
